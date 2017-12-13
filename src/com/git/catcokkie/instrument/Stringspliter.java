7fa5322a57484c32b351e7081a08c22a8e0b439e
zyf <761436715@qq.com>
2017-12-13 22:09:52 +0800

    本地修改完善，12/13

--- src/com/git/catcokkie/instrument/Stringspliter.java
+++ src/com/git/catcokkie/instrument/Stringspliter.java
@@ -7,11 +7,19 @@ package com.git.catcokkie.instrument;
  * @Time
  */
 public class Stringspliter {
-    public  Stringspliter(){
+    public Stringspliter( String a ) {
 
     }
-     public static String[] stringSpliter( String a){
-        String[] strarray=a.split(",");
-        return strarray;
-     }
+//     public static String[] stringSpliter(String a){
+//        String[] strarray=a.split(",");
+//        return strarray;
+//     }
+
+    public static void main( String[] args ) {
+        String[] lines = new FileReader("E:\\ simulate\\a\\b.txt", "GBK").getLines();
+        String[] testArray = lines[0].split(",");
+        for (int j = 0; j <= lines.length; j++) {
+            System.out.println("" + testArray);
+        }
+    }
 }
